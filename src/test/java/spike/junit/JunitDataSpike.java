44e4136a493f6eb91b3eee399acfd6867b47a4e9
hippoom <hippoom@gmail.com>
2016-10-13 15:06:18 +0800

    basic usage for junit data driven testing

---
+++ src/test/java/spike/junit/JunitDataSpike.java
@@ -0,0 +1,24 @@
+package spike.junit;
+
+import org.junit.Test;
+
+import static java.lang.Math.max;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class JunitDataSpike {
+
+    @Test
+    public void shouldReturn3_whenMax_given1And3() {
+        int expected = 3;
+
+        int actual = max(1, 3);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldReturn0_whenMax_givenDouble0() {
+        assertThat(max(0, 0), is(0));
+    }
+}
